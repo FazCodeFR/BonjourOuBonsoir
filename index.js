d149e826535605c7fd33f34d213aa9c306d34750
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2023-02-12 19:55:27 +0100

    Add css

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const bonTextEnum = {
 
 // Init 
 document.getElementById("bonText").innerHTML = bonText;
-getMeteo();
 
+
+
+// futur Feature 
+//getMeteo();
+
+calcul(6, 18)
 
 function getMeteo() {
   const urlApiMeteo = 'https://api.open-meteo.com/v1/meteofrance?latitude=48.85&longitude=2.35&hourly=temperature_2m&daily=sunrise,sunset&timezone=auto'
@@ -19,7 +24,9 @@ function getMeteo() {
     .then(data => {
       calcul(new Date(data.daily.sunrise[0]).getHours(), new Date(data.daily.sunset[0]).getHours());
     })
-    .catch(error => alert("Erreur : " + error));
+    .catch(error =>
+      calcul(6, 18)
+    );
 }
 
 function calcul(heureMatin, heureSoir) {
