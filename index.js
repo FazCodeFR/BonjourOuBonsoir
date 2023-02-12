677d63a43cdd7508c6b3c178521ac11e59e2706b
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2023-02-12 18:37:44 +0100

    Add icon

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let today = new Date();
 let bonText = "Bonjour"
-let sunset = 0
-let sunrise = 0
 
 const bonTextEnum = {
   Bonjour: "Bonjour",
@@ -12,11 +10,6 @@ const bonTextEnum = {
 document.getElementById("bonText").innerHTML = bonText;
 getMeteo();
 
-if (today.getHours() >= sunset && today.getHours() <= sunrise) {
-  bonText = bonTextEnum.Bonsoir
-}
-
-document.getElementById("bonText").innerHTML = bonText;
 
 function getMeteo() {
   const urlApiMeteo = 'https://api.open-meteo.com/v1/meteofrance?latitude=48.85&longitude=2.35&hourly=temperature_2m&daily=sunrise,sunset&timezone=auto'
@@ -24,9 +17,17 @@ function getMeteo() {
   fetch(urlApiMeteo)
     .then(response => response.json())
     .then(data => {
-      sunset = new Date(data.daily.sunset[0]).getHours()
-      sunrise = new Date(data.daily.sunrise[0]).getHours()
+      calcul(new Date(data.daily.sunrise[0]).getHours(), new Date(data.daily.sunset[0]).getHours());
     })
     .catch(error => alert("Erreur : " + error));
 }
 
+function calcul(heureMatin, heureSoir) {
+  if (heureMatin >= today.getHours() && today.getHours() <= heureSoir) {
+    bonText = bonTextEnum.Bonjour
+  }
+  else {
+    bonText = bonTextEnum.Bonsoir
+  }
+  document.getElementById("bonText").innerHTML = bonText;
+}
